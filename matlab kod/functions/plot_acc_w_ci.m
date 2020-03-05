23fdf9cdbdc58e32b828156a596079788985af08
theper <perwaaler@gmail.com>
2020-03-05 18:21:24 +0100

    saved a bunch of plots

--- matlab kod/functions/plot_acc_w_ci.m
+++ matlab kod/functions/plot_acc_w_ci.m
@@ -1,14 +1,22 @@
-function a = plot_acc_w_ci(data_type, transform, trans_par, p_true, cut_off, color, data_mark)
+function a = plot_acc_w_ci(data_type, transform, trans_par, p_true, cut_off, color, data_mark, u_u, u_l)
+if nargin==7 
+    pc = get_data(3, data_type, transform, trans_par, 1, 80, 6);
+    accuracy = accuracy_rate(pc, p_true, cut_off);
+    ci = compute_ci_pest(accuracy,500);
+    a=plot(100*accuracy, data_mark, 'color', color);
+    hold on
+    plot(100*accuracy,'color', color);
+    plot(100*ci,':','color', color)
 
-pc = get_data(3, data_type, transform, trans_par, 1, 80, 6);
-accuracy = accuracy_rate(pc, p_true, cut_off);
-ci = compute_ci_pest(accuracy,500);
-
-a=plot(100*accuracy, data_mark, 'color', color);
-hold on
-plot(100*accuracy,'color', color);
-plot(100*ci,':','color', color)
-
+else
+    pc = get_data(3, data_type, transform, trans_par, 1, u_u*100, u_l*100);
+    accuracy = accuracy_rate(pc, p_true, cut_off);
+    ci = compute_ci_pest(accuracy,500);
+    a=plot(100*accuracy, data_mark, 'color', color);
+    hold on
+    plot(100*accuracy,'color', color);
+    plot(100*ci,':','color', color)
+    
 end
 
 
